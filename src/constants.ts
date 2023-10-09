85cbbb22629624b5097c809e1757e4eaf1de4b03
椰云拿铁 <102728558+lin-stephanie@users.noreply.github.com>
2023-10-09 22:56:25 +0800

    fix: add excluded ext

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import type { UrlDisplaySettings } from "./types"
 export const VIEW_TYPE = "url-display";
 export const URLREGEX = /\[([^\]]*?)\]\((http[s]?:\/\/[^)]+)\)|(http[s]?:\/\/[^\s]+)/g;
 export const EXTERNAL_LINK = /(\[([^\[\]]+?)?\]\()?https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/='" ]*)\)?/g;
-export const EXCLUDE = ['.avif', '.apng','.jpg', '.jpeg', '.png', '.gif', '.svg', '.ico', '.bmp', '.webp', '.mp4', '.avi', '.mkv', '.mov', '.flv', '.webm', '.awebp?'];
+export const EXCLUDE = ['.avif', '.apng','.jpg', '.jpeg', '.png', '.gif', '.svg', '.ico', '.bmp', '.webp', '.mp4', '.avi', '.mkv', '.mov', '.flv', '.webm','.awebp', '.awebp?'];
 export const SPECIAL = /\?target=([^&\s]+)/;
 
 export const DEFAULT_SETTINGS: UrlDisplaySettings = {
